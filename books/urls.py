ba9433d3a98458404b683ebbdc947326de1b8f1d
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-11-05 11:41:58 +0500

    books detail

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import home_view, about_view
+from .views import home_view, books_view,books_detail_view
+
+
+app_name = "books"
 
 urlpatterns = [
-    path('',home_view,name='home pages'),
-    path('about/',about_view,name='about pages')
+    path('',home_view,name='home_pages'),
+    path('books/',books_view,name='books_pages'),
+    path('books/<int:book_id>',books_detail_view, name='detail')
 ]
--- books/views.py
+++ books/views.py
@@ -4,8 +4,47 @@ from django.shortcuts import render
 
 from django.http import HttpResponse,HttpRequest
 
+books = [
+{
+
+    'id':1,
+    'name': 'O\'tgan Kunlar',
+    'author': 'Abdulla Qodiriy'
+},
+{
+    'id':2,
+    'name': 'Dost ortirish',
+    'author': 'Deyl Karnegi'
+},
+{
+    'id':3,
+    'name': 'Atom odatlari',
+    'author': 'stev jops'
+},
+{
+    'id':4,
+    'name': 'Xamsa',
+    'author': 'Navoiy'
+}]
+
 def home_view(request: HttpRequest) -> HttpResponse:
     return render(request=request,template_name='home.html')
 
-def about_view(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('About ')
+def books_view(request: HttpRequest) -> HttpResponse:
+    context = {
+        'books':books,
+        'name': 'Ali'
+    }
+
+    return render(request=request,context=context,template_name='books.html')
+
+
+def books_detail_view(request: HttpRequest,book_id:int) -> HttpResponse:
+    for book in books:
+        if book['id'] == book_id:
+            context = {
+                'book':book
+            }
+            return render (request=request,template_name='book_detail.html',context=context)
+
+    return render(request=request,template_name='books.html') 
